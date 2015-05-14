f8595e988baad426f0a6355912122fa301773a85
Kpatena <kamilpatena1221@gmail.com>
2015-05-14 02:00:07 -0700

    Fixed Spawning

---
+++ Assets/Scripts/RaceCarSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceCarSpawner : MonoBehaviour {
+
+	public float timer = 10f;
+	public float repeat = 10f;
+	public int carsPerLevel;
+	private int randomCar = 1;
+	private int next = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		timer -= Time.deltaTime;
+
+		if (timer <= 0 && next <= carsPerLevel && randomCar == 1) {
+			string car = next.ToString();
+			randomCar = Random.Range (1,2);
+			GameObject go = Instantiate (Resources.Load ("YellowRaceCar")) as GameObject;
+			go.tag = "RaceCar" + car;
+			Debug.Log ("Car" + car);
+			go.GetComponent<Car1>().carTag = "RaceCar" + car;
+			timer = repeat;
+			next++;
+		}
+	}
+}
--- Assets/Scripts/Spawning.cs
+++ Assets/Scripts/Spawning.cs
@@ -6,7 +6,6 @@ public class Spawning : MonoBehaviour {
 	public float timer = 5f;
 	public float repeat = 5f;
 	public int carsPerLevel;
-	public int differentCarsInLevel = 3;
 	private int randomCar = 1;
 	private int next = 0;
 	//  respawns = GameObject.FindGameObjectsWithTag("Respawn");
@@ -21,32 +20,23 @@ public class Spawning : MonoBehaviour {
 
 		if (timer < 0 && next <= carsPerLevel && randomCar == 1) {
 			string car = next.ToString();
-			randomCar = Random.Range (1,differentCarsInLevel);
+			randomCar = Random.Range (1,3);
 			GameObject go = Instantiate (Resources.Load ("YellowCar")) as GameObject;
 			go.tag = "Car" + car;
 			Debug.Log ("Car" + car);
 			go.GetComponent<Car1>().carTag = "Car" + car;
 			timer = repeat;
-			next++;} 
-		else if (timer < 0 && next <= carsPerLevel && randomCar == 2) {
+			next++;
+		} else if (timer < 0 && next <= carsPerLevel && randomCar == 2) {
 			string car = next.ToString();
-			randomCar = Random.Range (1,differentCarsInLevel);
+			randomCar = Random.Range (1,3);
 			GameObject go = Instantiate (Resources.Load ("RedCar")) as GameObject;
 			go.tag = "Car" + car;
 			Debug.Log ("Car" + car);
 			go.GetComponent<Car1>().carTag = "Car" + car;
 			timer = repeat;
 			next++;
-		} else if (timer < 0 && next <= carsPerLevel && randomCar == 3) {
-			string car = next.ToString();
-			randomCar = Random.Range (1,differentCarsInLevel);
-			GameObject go = Instantiate (Resources.Load ("YellowRaceCar")) as GameObject;
-			go.tag = "RaceCar" + car;
-			Debug.Log ("Car" + car);
-			go.GetComponent<Car1>().carTag = "Car" + car;
-			timer = repeat;
-			next++;
-		}
+		} 
 
 	}
 }
