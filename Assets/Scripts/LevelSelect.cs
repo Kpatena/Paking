2d3c71b3a69b807f91999cf4e2e5798d389a54da
Tony Lee <zapman89@hotmail.com>
2015-05-20 02:36:07 -0700

    Added dank level and scaled level select Level Select buttons are linked, just write the names of your level on the canvas

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -9,6 +9,14 @@ public class LevelSelect : MonoBehaviour {
 
 	public string level1;
 	public string level2;
+	public string level3;
+	public string level4;
+	public string level5;
+	public string level6;
+	public string level7;
+	public string level8;
+	public string level9;
+	public string level10;
 	public string mainMenu;
 	public static bool didItQuit;
 
@@ -22,10 +30,47 @@ public class LevelSelect : MonoBehaviour {
 		Time.timeScale = 1;
 	}
 
-	public void PressedBack(){
-		Application.LoadLevel (mainMenu);
+	public void LevelThree(){
+		Application.LoadLevel (level3);
+		Time.timeScale = 1;
 	}
 
-	public void LevelLock(){
+	public void LevelFour(){
+		Application.LoadLevel (level4);
+		Time.timeScale = 1;
+	}
+
+	public void LevelFive(){
+		Application.LoadLevel (level5);
+		Time.timeScale = 1;
+	}
+
+	public void LevelSix(){
+		Application.LoadLevel (level6);
+		Time.timeScale = 1;
+	}
+
+	public void LevelSeven(){
+		Application.LoadLevel (level7);
+		Time.timeScale = 1;
+	}
+
+	public void LevelEight(){
+		Application.LoadLevel (level8);
+		Time.timeScale = 1;
+	}
+
+	public void LevelNine(){
+		Application.LoadLevel (level9);
+		Time.timeScale = 1;
+	}
+
+	public void LevelTen(){
+		Application.LoadLevel (level10);
+		Time.timeScale = 1;
+	}
+
+	public void PressedBack(){
+		Application.LoadLevel (mainMenu);
 	}
 }
