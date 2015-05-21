e7506379ae74a88fc9256151295dead073ae112c
Tony Lee <zapman89@hotmail.com>
2015-05-20 23:15:40 -0700

    Added Level Select Screenshots

---
+++ Assets/Scripts/RotateText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateText : MonoBehaviour {
+
+	int counter = 0;
+	public GameObject button;
+
+	// Update is called once per frame
+	void Update () {
+		if (counter < 10) {
+			button.transform.Rotate (Vector3.right * Time.deltaTime);
+			counter++;
+		}
+		if (counter > 10) {
+			button.transform.Rotate (Vector3.left * Time.deltaTime);
+			counter++;
+			if (counter == 20){
+				counter = 0;
+			}
+		}
+
+	}
+}
