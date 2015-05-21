2ff54116b5fb6a7c6a0be66d598466498f837c54
Tony Lee <zapman89@hotmail.com>
2015-05-20 23:46:20 -0700

    Added a loading screen

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class LoadingScreen : MonoBehaviour {
+	
+	Canvas canvas;
+	
+	void Start()
+	{
+		// sets canvas to variable
+		canvas = GetComponent<Canvas>();
+		canvas.enabled = false;
+	}
+	
+	public void showLoading(){
+		canvas.enabled = !canvas.enabled;
+	}
+}
