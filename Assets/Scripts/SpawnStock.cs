28627a59ec276189b1493118c75bdaff9a04f1e9
Wesley Sura <wjsura95@gmail.com>
2015-05-20 17:43:53 -0700

    new level. Not sure why, but time won't tick down for me.

---
+++ Assets/Scripts/SpawnStock.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SpawnStock : MonoBehaviour {
+	
+	public string whichRedPrefab;
+	public string whichYellowPrefab;
+	public float timer = 5f;
+	public float repeat = 5f;
+	public int carsPerLevel;
+	public Transform[] spawnPoints;
+	private int spawnPointIndex;
+	private int randomCar = 0;
+	private int next = 0;
+	//  respawns = GameObject.FindGameObjectsWithTag("Respawn");
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		
+		if (timer < 0 && next <= 60) {
+			//string car = next.ToString();
+			//randomCar = Random.Range (0,2);
+			spawnPointIndex = Random.Range (0,spawnPoints.Length);
+			//make prefab for car and store in resources folder
+			Instantiate (Resources.Load ("StockCar"), spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+			//go.tag = "Wall";
+			timer = repeat;
+			next++;
+		}		
+	}
+}
---
+++ Assets/Scripts/StockCarMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StockCarMove : MonoBehaviour {
+
+	public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (Vector3.up * movementSpeed * Time.deltaTime); 
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "BoxCollider") {
+			Destroy (this.gameObject);
+		}
+		if (other.tag == "Wall") {
+			Destroy (this.gameObject);
+		}
+	}
+}
